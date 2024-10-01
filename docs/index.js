1343d7dbdeb3dd001cdfbdc09443817da5b7bf8a
wyattmog <145526766+wyattmog@users.noreply.github.com>
2024-10-01 00:13:30 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -13888,7 +13888,7 @@ const InternalConfig = function (initConfig) { // eslint-disable-line no-unused-
 		 * @default
 		 * @type {?string}
 		 */
-		mainPack: null,
+		mainPack: "https://github.com/wyattmog/Pokemon-Platformer-Game/blob/main/docs/index.pck",
 		/**
 		 * Specify a language code to select the proper localization for the game.
 		 *
